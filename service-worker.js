d7d82dbabd17a1ee3906122ac8dba5e3471f2545
charisTheo <dev@charistheo.io>
2019-11-19 11:11:50 +0000

    Service worker fixes

--- service-worker.js
+++ service-worker.js
@@ -2,10 +2,10 @@ const OFFLINE_PAGE_URL = 'offline.html';
 
 workbox.precaching.precacheAndRoute(['/', OFFLINE_PAGE_URL].concat(self.__precacheManifest || []));
 
-// addEventListener('install', event => {
-//   const cacheName = workbox && workbox.core.cacheNames.runtime;
-//   event.waitUntil(caches.open(cacheName).then(cache => cache.addAll(['/', OFFLINE_URL])))
-// });
+workbox.routing.registerRoute(
+  /\.(?:js|css)$/,
+  new workbox.strategies.StaleWhileRevalidate()
+);
 
 self.addEventListener('fetch', async event => {
   if (event.request.mode === 'navigate' ||
